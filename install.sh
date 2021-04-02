0345bb496963f2343bd7803e44ad7acc35926c28
Twe3x <holl.simon20021@gmail.com>
2021-04-03 00:07:14 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+clear
+echo installling gti...
+sleep 2
+sudo apt install git screen -y
+
+clear
+
+read -p "Enter the server name: " name
+
+dir=/home/Spigot/$name/
+
+if [ -e /home/Spigot/ ]
+then
+	sleep 1
+else
+	mkdir /home/Spigot/
+fi
+
+if [ -e $dir ]
+then
+	sleep 1
+else
+	mkdir $dir
+fi
+
+cd $dir
+echo '#By changing the setting below to TRUE you are indicating your agreement to our EULA (https://account.mojang.com/documents/minecraft_eula).
+#Fri Apr 02 23:31:20 CEST 2021
+eula=true' >$dir/eula.txt
+
+if [ -e $dir/BuildTools/ ]
+then
+	sleep 1
+else
+	mkdir $dir/BuildTools/
+fi
+cd $dir/BuildTools/
+
+# Download latest BuildTools, ask for the Spigot version (default is latest), and run BuildTools.
+curl -z BuildTools.jar -o BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
+
+clear
+
+read -p "Enter the version: " version
+java -jar BuildTools.jar --rev ${version:-latest}
+
+mv spigot-$version.jar ../
+rm -rf $dir/BuildTools/
+
+clear
+
+echo screen -dmS $name java -jar spigot-$version.jar >$dir/start.sh
+
+echo "Server was created successfully"
